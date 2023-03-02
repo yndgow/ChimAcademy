a133d0c85d79299347ae1ffce60316daf994d214
yndgow <yndgow@gmail.com>
2023-03-02 12:05:36 +0900

    처음 폴더구성 패키지 구성

---
+++ src/main/java/kr/co/GreenAcademy/GreenAcademyApplication.java
@@ -0,0 +1,13 @@
+package kr.co.GreenAcademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenAcademyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenAcademyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/GreenAcademy/GreenAcademyApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.GreenAcademy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenAcademyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
