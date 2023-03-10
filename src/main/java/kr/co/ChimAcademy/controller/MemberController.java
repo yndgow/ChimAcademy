4dca06d613af75aa5a535dde6f3129b3ad3f38fe
haerisogyumo <haerisogyumo@gmail.com>
2023-03-10 13:47:17 +0900

    푸터 작업중

---
+++ src/main/java/kr/co/ChimAcademy/controller/MemberController.java
@@ -0,0 +1,14 @@
+package kr.co.ChimAcademy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MemberController {
+	
+	@GetMapping("member/join")
+	public String join() {
+		return "member/join";
+	}
+
+}
