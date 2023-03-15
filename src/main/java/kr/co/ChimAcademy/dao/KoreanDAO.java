45b473fae7d64c6aa75d0e08f988810b49d89087
Hoongithub2022 <gns2506@gmail.com>
2023-03-15 13:36:58 +0900

    국문학과 게시판 작업

--- src/main/java/kr/co/ChimAcademy/dao/KoreanDAO.java
+++ src/main/java/kr/co/ChimAcademy/dao/KoreanDAO.java
@@ -11,11 +11,17 @@ import kr.co.ChimAcademy.vo.BoardVO;
 @Mapper
 public interface KoreanDAO {
 	// 국문학과 게시판 목록 출력(List)
-	public List<BoardVO> selectBoards();
-	
-	// 국문학과 게시판 작성 출력(Write)
-	public int insertBoard(BoardVO vo);
+	public List<BoardVO> selectBoards(int start);
 	
 	// 국문학과 게시판 보기(view)
 	public BoardVO selectBoard(int no);
+	
+	// 국문학과 게시판 작성(Write)
+	public int insertBoard(BoardVO vo);
+	
+	// 국문학과 게시판글 삭제(Delete)
+	public int deleteBoard(int no);
+	
+	// 국문학과 게시판 페이징
+	public int selectCount();
 }
