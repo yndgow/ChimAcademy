a3268256bcc92d4e9c47b6a89832c9921c40f496
yndgow <yndgow@gmail.com>
2023-03-20 13:55:33 +0900

    로그인 페이지 수정

--- src/main/java/kr/co/ChimAcademy/controller/MainController.java
+++ src/main/java/kr/co/ChimAcademy/controller/MainController.java
@@ -1,16 +1,20 @@
 package kr.co.ChimAcademy.controller;
 
+
+import java.security.Principal;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
 @Controller
 public class MainController {
 	
 	@GetMapping(value = {"/", "/index"})
-	public String index(Model model, @CookieValue(name = "remember-me", required = false) String remebermeCookie) {
-		if(remebermeCookie!= null) {
+	public String index(Model model, @CookieValue(name = "remember-me", required = false) String remebermeCookie, Principal principal) {
+		if(remebermeCookie!= null || principal != null) {
 			return "redirect:/notice";
 		}else {
 			System.out.println("로그인페이지로");
